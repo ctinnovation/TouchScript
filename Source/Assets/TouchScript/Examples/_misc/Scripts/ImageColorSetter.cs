22569d34b1782675bbe4294bea3e664abadd696b
Jorrit de Vries <jorrit@ijsfontein.nl>
2023-04-05 14:50:11 +0200

    Adds setup to test long press gestures

---
+++ Source/Assets/TouchScript/Examples/_misc/Scripts/ImageColorSetter.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageColorSetter : MonoBehaviour
+{
+    [SerializeField] private Image image;
+    [SerializeField] private Color[] colors;
+
+    public void SetColor(int index)
+    {
+        if (index < 0 || index >= colors.Length)
+        {
+            Debug.LogWarning($"{this}: SetColor index out of bounds");
+            return;
+        }
+        
+        SetColor(colors[index]);
+    }
+
+    public void SetColor(Color value)
+    {
+        image.color = value;
+    }
+}
